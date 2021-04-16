602b54352b6c87d2a3c2e839d5389ccf61372ccb
Cruhrup <christopher.ruhrup@gmail.com>
2021-04-16 09:23:13 -0500

    accidentally forgot to add the modified instances.tf and variables.tf to last commit

--- variables.tf
+++ variables.tf
@@ -18,3 +18,12 @@ variable "external_ip" {
   default = "0.0.0.0/0"
 }
 
+variable "workers-count" {
+  type    = number
+  default = 1
+}
+
+variable "instance-type" {
+  type    = string
+  default = "t3.micro"
+}
